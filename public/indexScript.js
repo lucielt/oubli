06bbba637da67b07017a35a65f0f39948631c508
Lucie Le Touze <lucieletouze@gmail.com>
2016-04-11 15:27:03 -0400

    define area which need to be paint under the rest

--- public/indexScript.js
+++ public/indexScript.js
@@ -11,6 +11,11 @@ canvas2.width = canvas.width;
 canvas2.height = canvas.height;
 var ctx2 = canvas2.getContext("2d");
 
+var canvas3 = document.getElementById("canvas3");
+canvas3.width = canvas.width;
+canvas3.height = canvas.height;
+var ctx3 = canvas3.getContext("2d");
+
 // ------ IMG
 var img = new Image();
 img.src = 'bkg.png';
@@ -29,8 +34,12 @@ var audio = document.getElementById("prout");
 
 
 img.onload = function() {
+    ctx.fillStyle = "white";
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.fill();
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(img2, 0, 0, canvas.width, canvas.height);
+    //draw();
 };
 
 var socket = io.connect('http://localhost:8080');
@@ -111,6 +120,9 @@ var sensors = [{
     cellWidth = 4,
     pas=0;*/
     var pas,
+        pas2,
+        nbCell,
+        cellWidth = 4,
         color,
         xInit,
         yInit,
@@ -137,6 +149,7 @@ $(document).keydown(function(e) {
     //delay(function(){
         for (i = 0; i < sensors.length; i++) {
             if (s == sensors[i].touches){
+                //drawSquare();
                 //radius = sensors[i].radius;
                 xInit = sensors[i].centerX;
                 yInit = sensors[i].centerY;
@@ -149,12 +162,12 @@ $(document).keydown(function(e) {
                 pointY = sensors[i].pointY;
                 color = getRandomColor();
                 //color = sensors[i].color[Math.floor((Math.random() * 5) + 1)];
-                /*nbCell = sensors[i].nbRow;
                 //console.log("Nom de la touche ="+sensors[i].touches);
                 //drawForm();
-                drawSquare();
+                /*drawSquare();
                 sensors[i].nbRow += 2;
-                pas += 1;
+                pas2 += 1;
+                nbCell = sensors[i].nbRow;
                 console.log("nbRow" + nbCell);*/
                 //xInit = window.innerWidth / 2;
                 //yInit = window.innerHeight / 2;
@@ -167,6 +180,7 @@ $(document).keydown(function(e) {
                 xDraw = xInit-(width/2);
                 yDraw = yInit-(height/2);
                 console.log("xDraw", xDraw ,"yDraw", yDraw);
+                //draw();
             }
         }
     //}, 1000 );
@@ -200,11 +214,11 @@ function drawColors() {
     imageData = ctx2.putImageData(imageData, xDraw, yDraw);
     ctx2.save();
     ctx2.beginPath();
-    //ctx2.arc(xInit, yInit, width/2, 0, 2 * Math.PI);
-    ctx2.moveTo(pointXinit, pointYinit);
+    ctx2.arc(xInit, yInit, width/2, 0, 2 * Math.PI);
+    /*ctx2.moveTo(pointXinit, pointYinit);
     for (j=1; j< pointXinit.length; j++){
         ctx2.lineTo(pointXinit[j], pointYinit[j]);
-    }
+    }*/
     ctx2.closePath();
     ctx2.clip();
     ctx2.drawImage(img2, 0, 0, canvas2.width, canvas2.height);
@@ -215,13 +229,11 @@ function drawColors() {
     ctx2.shadowColor="rgba(200,200,200,100)";
     ctx2.fillStyle = color;
     ctx2.beginPath();
-    //ctx2.arc(xInit, yInit, width/2, 0, 2 * Math.PI);
-    ctx2.moveTo(pointXinit, pointYinit);
+    ctx2.arc(xInit, yInit, width/2, 0, 2 * Math.PI);
+    /*ctx2.moveTo(pointXinit, pointYinit);
     for (j=1; j< pointX.length; j++){
         ctx2.lineTo(pointX[j], pointY[j]);
-        console.log("pointX", pointX[j]);
-        console.log("pointY", pointY[j]);
-    }
+    }*/
     ctx2.closePath();
     /*ctx2.beginPath();
     ctx2.arc(xInit, yInit, width/2, 0, 2 * Math.PI);
@@ -229,6 +241,21 @@ function drawColors() {
     ctx2.fill();
     xDraw = xInit-(width/2);
     yDraw = yInit-(height/2);
+    var imgPointX =[0, 110, 142, 165, 171, 125, 106, 218, 234, 273, 285, 285, 256, 223, 200, 183, 177, 186, 165, 221, 201, 186, 189, 225, 265, 285, 336, 340, 400, 451, 502, 576, 576, 534, 497, 462, 440, 393, 374, 352, 346, 350, 360, 355, 441, 441, 448, 433, 458, 464, 472, 482, 523, 538, 576, 336, 336, 325, 322, 301, 301, 246, 219, 215, 191, 167, 145, 133, 125, 134, 141, 134, 138, 134, 129, 126, 108, 91, 88, 84, 77, 0];
+    var imgPointY = [0, 0, 42, 67, 85, 131, 174, 160, 158, 138, 115, 83, 99, 98, 87, 71, 49, 40, 0, 0, 33, 51, 68, 91, 87, 74, 55, 114, 183, 190, 158, 154, 200, 171, 175, 197, 200, 192, 185, 205, 258, 292, 321, 339, 214, 206, 210, 288, 318, 317, 338, 336, 373, 373, 384, 384, 351, 337, 324, 306, 299, 274, 294, 303, 299, 294, 307, 310, 318, 329, 339, 352, 384, 384, 374, 329, 326, 300, 263, 253, 217, 164];
+    var long = imgPointX.length;
+    ctx2.save();
+    ctx2.globalCompositeOperation = "normal";
+    ctx2.beginPath();
+    ctx2.moveTo(imgPointX[0], imgPointY[0]);
+    for (j=1; j< imgPointX.length; j++){
+        ctx2.lineTo(imgPointX[j], imgPointY[j]);
+        //console.log("jesuisla", imgPointX[j], imgPointY[j]);
+    }
+    //ctx3.closePath();
+    ctx2.clip();
+    ctx2.drawImage(img3, 0, 0, canvas2.width, canvas2.height);
+    ctx2.restore();
 }
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
@@ -238,14 +265,31 @@ function getRandomColor() {
     }
     return color;
 }
-/*
-function drawSquare(){
+function draw(){
+    var imgPointX =[0, 110, 142, 165, 171, 125, 106, 218, 234, 273, 285, 285, 256, 223, 200, 183, 177, 186, 165, 221, 201, 186, 189, 225, 265, 285, 336, 340, 400, 451, 502, 576, 576, 534, 497, 462, 440, 393, 374, 352, 346, 350, 360, 355, 441, 441, 448, 433, 458, 464, 472, 482, 523, 538, 576, 336, 336, 325, 322, 301, 301, 246, 219, 215, 191, 167, 145, 133, 125, 134, 141, 134, 138, 134, 129, 126, 108, 91, 88, 84, 77, 0];
+    var imgPointY = [0, 0, 42, 67, 85, 131, 174, 160, 158, 138, 115, 83, 99, 98, 87, 71, 49, 40, 0, 0, 33, 51, 68, 91, 87, 74, 55, 114, 183, 190, 158, 154, 200, 171, 175, 197, 200, 192, 185, 205, 258, 292, 321, 339, 214, 206, 210, 288, 318, 317, 338, 336, 373, 373, 384, 384, 351, 337, 324, 306, 299, 274, 294, 303, 299, 294, 307, 310, 318, 329, 339, 352, 384, 384, 374, 329, 326, 300, 263, 253, 217, 164];
+    var long = imgPointX.length;
+    ctx3.fillStyle = 'white';
+    ctx3.beginPath();
+    ctx3.moveTo(imgPointX[0], imgPointY[0]);
+    for (j=1; j< imgPointX.length; j++){
+        ctx3.lineTo(imgPointX[j], imgPointY[j]);
+        //console.log("jesuisla", imgPointX[j], imgPointY[j]);
+    }
+    //ctx3.closePath();
+    ctx3.fill();
+    ctx3.clip();
+    ctx3.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+    ctx3.drawImage(img2, 0, 0, canvas2.width, canvas2.height);
+}
+
+/*function drawSquare(){
     for (var row = 0; row < nbCell; row ++){
         for (var column = 0; column < nbCell; column ++){
             // coordinates of the top-left corner
-            var x = (centerx - (cellWidth*pas)) + column * 4;
-            var y = (centery - (cellWidth*pas)) + row * 4;
-            console.log("X " + x + " Y " + y);
+            var x1 = (xInit - (cellWidth*pas2)) + column * 4;
+            var y2 = (yInit - (cellWidth*pas2)) + row * 4;
+            console.log("X " + x1 + " Y " + y2);
 
             if (row%2 == 0){
                 if (column%2 == 0){
@@ -263,7 +307,7 @@ function drawSquare(){
                     console.log(ctx2.fillStyle);
                 }
             }
-            ctx2.fillRect(x, y, cellWidth, cellWidth);
+            ctx2.fillRect(x1, y2, cellWidth, cellWidth);
         }
     }
 }*/
