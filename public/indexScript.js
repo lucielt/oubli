4bf638ae97cd76aff4f548eabd801af1d8d266bb
Lucie Le Touze <lucieletouze@gmail.com>
2016-04-10 20:12:05 -0400

    add circle moiree & arduino keyboard

--- public/indexScript.js
+++ public/indexScript.js
@@ -2,37 +2,37 @@
 
 // ------ CANVAS
 var canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 576;
+canvas.height = 384;
 var ctx = canvas.getContext("2d");
 
-//var windowSize = c.height*c.width;
-
-var c2 = document.getElementById("canvas2");
-c2.width = canvas.width;
-c2.height = canvas.height;
-var ctx2 = c2.getContext("2d");
-
-// ------ AUDIO
-var audio = document.getElementById("prout");
+var canvas2 = document.getElementById("canvas2");
+canvas2.width = canvas.width;
+canvas2.height = canvas.height;
+var ctx2 = canvas2.getContext("2d");
 
 // ------ IMG
 var img = new Image();
 img.src = 'bkg.png';
 
-var img2 = new Image(),
-    imgData;
+var img2 = new Image();
 img2.src = 'rose.png';
+
+var img3 = new Image(),
+    imgData;
+img3.src = 'telechargement.png';
+
+// ------ AUDIO
+var audio = document.getElementById("prout");
 
 // ----------------------------------------------------------------------------
 
 function draw() {
     img.onload = function() {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        ctx2.drawImage(img2, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img2, 0, 0, canvas.width, canvas.height);
     }
 }
-
 draw();
 
 var socket = io.connect('http://localhost:8080');
@@ -42,73 +42,172 @@ var socket = io.connect('http://localhost:8080');
 var sensors = [{
     data: '0',
     color: ['#FDF2FF', '#FFF2F2', '#EFEDFC', '#F2FFEA', '#F2FFFE', '#FCFCE9'],
-    centerX: 0,
-    centerY: 200,
+    centerX: (canvas2.width / 2)-200,
+    centerY: canvas2.height / 2,
+    width: 10,
+    height: 10,
+    pas : 20,
     radius: 50,
     touches: 'Þ',
     nbRow:2
 }, {
     data: '1',
     color: ['#FDF2FF', '#FFF2F2', '#EFEDFC', '#F2FFEA', '#F2FFFE', '#FCFCE9'],
-    centerX: 800,
-    centerY: 50,
+    centerX: (canvas2.width / 2)+200,
+    centerY: canvas2.height / 2,
+    width: 10,
+    height: 10,
+    pas : 20,
     radius: 100,
     touches: 'À',
     nbRow:2
 }, {
     data: '2',
     color: ['#FDF2FF', '#FFF2F2', '#EFEDFC', '#F2FFEA', '#F2FFFE', '#FCFCE9'],
-    centerX: 500,
-    centerY: 150,
+    centerX: canvas2.width / 2,
+    centerY: canvas2.height / 2,
+    width: 10,
+    height: 10,
+    pas : 30,
     radius: 100,
     touches: '3',
     nbRow:2
 }, {
     data: '3',
     color: ['#FDF2FF', '#FFF2F2', '#EFEDFC', '#F2FFEA', '#F2FFFE', '#FCFCE9'],
-    centerX: 250,
-    centerY: 400,
+    centerX: canvas2.width / 2,
+    centerY: (canvas2.height / 2)-200,
+    width: 10,
+    height: 10,
+    pas : 40,
     radius: 5,
     touches: '5',
     nbRow:2
 }, {
     data: '4',
     color: ['#FDF2FF', '#FFF2F2', '#EFEDFC', '#F2FFEA', '#F2FFFE', '#FCFCE9'],
-    centerX: 300,
-    centerY: 500,
+    centerX: canvas2.width / 2,
+    centerY: (canvas2.height / 2)+200,
+    width: 10,
+    height: 10,
+    pas : 50,
     radius: 10,
     touches: '8',
     nbRow:2
 }, ];
 
-var nbCell,
+/*var nbCell,
     color,
     centerx,
     centery,
     cellWidth = 4,
-    pas=0;
-
-
+    pas=0;*/
+    var pas,
+        color,
+        xInit,
+        yInit,
+        width,
+        height,
+        xDraw = xInit-(width/2),
+        yDraw = yInit-(height/2);
+    var delay = (function(){
+        var timer = 0;
+        return function(callback, ms){
+            clearTimeout (timer);
+            timer = setTimeout(callback, ms);
+        };
+    })();
 $(document).keydown(function(e) {
     var s = String.fromCharCode(e.which);
     console.log(s);
-    for (i = 0; i < sensors.length; i++) {
-        if (s == sensors[i].touches){
-            //radius = sensors[i].radius;
-            centerx = sensors[i].centerX;
-            centery = sensors[i].centerY;
-            color = sensors[i].color[Math.floor((Math.random() * 5) + 1)];
-            nbCell = sensors[i].nbRow;
-            //console.log("Nom de la touche ="+sensors[i].touches);
-            //drawForm();
-            drawSquare();
-            sensors[i].nbRow += 2;
-            pas += 1;
-            console.log("nbRow" + nbCell);
+    //delay(function(){
+        for (i = 0; i < sensors.length; i++) {
+            if (s == sensors[i].touches){
+                //radius = sensors[i].radius;
+                xInit = sensors[i].centerX;
+                yInit = sensors[i].centerY;
+                width = sensors[i].width;
+                height = sensors[i].height;
+                pas = sensors[i].pas;
+                color = getRandomColor();
+                //color = sensors[i].color[Math.floor((Math.random() * 5) + 1)];
+                /*nbCell = sensors[i].nbRow;
+                //console.log("Nom de la touche ="+sensors[i].touches);
+                //drawForm();
+                drawSquare();
+                sensors[i].nbRow += 2;
+                pas += 1;
+                console.log("nbRow" + nbCell);*/
+                //xInit = window.innerWidth / 2;
+                //yInit = window.innerHeight / 2;
+                xDraw = xInit-(width/2);
+                yDraw = yInit-(height/2);
+                drawColors();
+                sensors[i].pas = pas * Math.pow(0.9, (1 / 16) * pas);
+                sensors[i].width += 0.5;
+                sensors[i].height += 0.5;
+                xDraw = xInit-(width/2);
+                yDraw = yInit-(height/2);
+                console.log("xDraw", xDraw ,"yDraw", yDraw);
+            }
+        }
+    //}, 1000 );
+});
+function drawColors() {
+    imageData = ctx2.createImageData(width, height);
+    //pos = 0; // index position into imagedata array
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            index = (x + (y * imageData.width)) * 4;
+            // calculate sine based on distance
+            /*x2 = x;
+            y2 = y;*/
+            d = Math.sqrt(Math.pow(x, 3) + Math.pow(y, 3));
+            t = Math.sin(d / pas);
+            // calculate RGB values based on sine
+            r = 105 + t * (Math.floor(Math.random() * 255));
+            g = 103 + t * (Math.floor(Math.random() * 255));
+            b = 100 + t * (Math.floor(Math.random() * 255));
+            if(Math.pow((x+xDraw) - xInit, 2) + Math.pow((y+yDraw) - yInit, 2) < Math.pow((width/2),2)){
+                imageData.data[index + 3] = 255;
+            } else {
+                imageData.data[index + 3] = 0;
+            }
+            imageData.data[index + 0] = r;
+            imageData.data[index + 1] = g;
+            imageData.data[index + 2] = b + 50;
+            //imageData.data[index + 3] = a;
         }
     }
-});
-
+    imageData = ctx2.putImageData(imageData, xDraw, yDraw);
+    ctx2.globalCompositeOperation = "overlay";
+    ctx2.save();
+    ctx2.beginPath();
+    ctx2.arc(xInit, yInit, width/2, 0, 2 * Math.PI);
+    ctx2.closePath();
+    ctx2.clip();
+    ctx2.drawImage(img2, 0, 0, canvas2.width, canvas2.height);
+    ctx2.restore();
+    //ctx2.globalCompositeOperation = "screen";
+    ctx2.shadowBlur=5;
+    ctx2.shadowColor="rgba(200,200,200,100)";
+    ctx2.fillStyle = color;
+    ctx2.beginPath();
+    ctx2.arc(xInit, yInit, width/2, 0, 2 * Math.PI);
+    ctx2.closePath();
+    ctx2.fill();
+    xDraw = xInit-(width/2);
+    yDraw = yInit-(height/2);
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+/*
 function drawSquare(){
     for (var row = 0; row < nbCell; row ++){
         for (var column = 0; column < nbCell; column ++){
@@ -136,164 +235,4 @@ function drawSquare(){
             ctx2.fillRect(x, y, cellWidth, cellWidth);
         }
     }
-}
-
-/*
-var angStep = 30;
-var lastx = null;
-var lasty = null;
-var lastcpx = null;
-var lastcpy = null;
-var bezierInt = false;
-var x,
-    y,
-    cpx,
-    cpy,
-    ang,
-    radius,
-    centerx,
-    centery,
-    color;
-
-$(document).keydown(function(e) {
-    var s = String.fromCharCode(e.which);
-    console.log(s);
-    for (i = 0; i < sensors.length; i++) {
-        if (s == sensors[i].touches){
-            radius = sensors[i].radius;
-            centerx = sensors[i].centerX;
-            centery = sensors[i].centerY;
-            color = sensors[i].color[Math.floor((Math.random() * 5) + 1)];
-            //console.log("Nom de la touche ="+sensors[i].touches);
-            drawForm();
-        }
-    }
-});
-
-function drawForm() {
-    console.log('je suis la');
-    ctx2.beginPath();
-    var startStep = (Math.random() * 360);
-    //var angleStep = 5;
-    radius += 0.5;
-    for (ang = startStep; ang <= 360 + startStep; ang += angStep) {
-        var rad = ang * Math.PI / 180;
-        var noise = (Math.random() * 200) + 1;
-        x = centerx + (radius * Math.cos(rad));
-        y = centery + (radius * Math.sin(rad));
-        cpx = centerx + ((radius + noise) * Math.cos(rad));
-        cpy = centery + ((radius + noise) * Math.sin(rad));
-        cpx2 = centerx + ((radius - (noise / 3)) * Math.cos(rad));
-        cpy2 = centery + ((radius - (noise / 3)) * Math.sin(rad));
-        if (lastx === null) {
-            ctx2.moveTo(x, y);
-            bezierInt = true;
-        } else if (bezierInt === false) {
-            //ctx.lineTo(x,y);
-            ctx2.bezierCurveTo(lastcpx, lastcpy, cpx, cpy, x, y);
-            bezierInt = true;
-        } else if (bezierInt === true) {
-            //ctx.lineTo(x,y);
-            ctx2.bezierCurveTo(lastcpx2, lastcpy2, cpx2, cpy2, x, y);
-            bezierInt = false;
-        }
-        lastx = x;
-        lasty = y;
-        lastcpx = cpx;
-        lastcpy = cpy;
-        lastcpx2 = cpx2;
-        lastcpy2 = cpy2;
-    }
-    //ctx.globalAlpha = Math.random();
-    ctx2.globalAlpha = 0.5;
-    ctx2.strokeStyle = color;
-    var pattern = ctx2.createPattern(img2, "repeat");
-    ctx2.fillStyle = pattern;
-    console.log(ctx2.strokeStyle);
-    ctx2.lineWidth = Math.random() * 5;
-    //ctx2.stroke();
-    ctx2.fill();
-}
-*/
-/*
-// when new data comes in the websocket, read it:
-socket.on('sensor', function(data) {
-      console.log(data);
-      var i = parseFloat(data);
-      var radius = sensors[i].radius;
-      var centerx = sensors[i].centerX;
-      var centery = sensors[i].centerY;
-      //console.log(typeof i);
-      //console.log(sensors[i].data);
-      //ctx.globalCompositeOperation = sensors[i].color;
-    function drawForm(){
-        ctx2.beginPath();
-        var startStep = (Math.random()*360);
-        //var angleStep = 5;
-        radius += 0.5;
-        for(ang = startStep; ang <= 360 + startStep; ang += angStep){
-            var rad = ang * Math.PI / 180;
-            var noise = (Math.random() * 200)+1;
-            x = centerx + (radius * Math.cos(rad));
-            y = centery + (radius * Math.sin(rad));
-            cpx = centerx + ((radius+noise) * Math.cos(rad));
-            cpy = centery + ((radius+noise) * Math.sin(rad));
-            cpx2 = centerx + ((radius-(noise/3)) * Math.cos(rad));
-            cpy2 = centery + ((radius-(noise/3)) * Math.sin(rad));
-            if(lastx === null){
-                ctx2.moveTo(x,y);
-                bezierInt = true;
-            }
-            else if (bezierInt === false){
-                //ctx.lineTo(x,y);
-                ctx2.bezierCurveTo(lastcpx, lastcpy, cpx, cpy, x, y);
-                bezierInt = true;
-                }
-                else if (bezierInt === true) {
-                    //ctx.lineTo(x,y);
-                    ctx2.bezierCurveTo(lastcpx2, lastcpy2, cpx2, cpy2, x, y);
-                    bezierInt = false;
-                }
-                lastx = x;
-                lasty = y;
-                lastcpx = cpx;
-                lastcpy = cpy;
-                lastcpx2 = cpx2;
-                lastcpy2 = cpy2;
-                }
-                //ctx.globalAlpha = Math.random();
-                ctx2.globalAlpha = 0.5;
-                ctx2.strokeStyle = sensors[i].color[Math.floor((Math.random() * 5)+1)];
-                var pattern = ctx2.createPattern(img2, "repeat");
-                ctx2.fillStyle = pattern;
-                  console.log(ctx2.strokeStyle);
-                ctx2.lineWidth = Math.random() * 5;
-                //ctx2.stroke();
-                ctx2.fill();
-                }
-                drawForm();
-
-                /*function drawCircle() {
-                    var j = Math.floor((Math.random() * 4) + 1);
-                    var k = Math.floor((Math.random() * 3) + 1);
-
-                  ctx2.beginPath();
-                  var x = (sensors[i].x)+100; // x coordinate
-                  var y = (sensors[i].y)+100; // y coordinate
-                  var startAngle = 0; // Starting point on circle
-                  var endAngle = Math.PI+(Math.PI*k)/2; // End point on circle
-                  var anticlockwise = j%2==0 ? false : true; // clockwise or anticlockwise
-
-                  ctx2.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-
-                  if (j>1){
-                  ctx2.fillStyle = sensors[i].color;
-                    ctx2.fill();
-                  } else {
-                      ctx2.fillStroke = sensors[i].color;
-                    ctx2.stroke();
-                  }
-                  radius= radius+0.1;
-            }
-            drawCircle();
-});*/
+}*/
